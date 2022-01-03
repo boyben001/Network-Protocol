6d788e9c5bba915f93ed311be5d209404a8525a3
BoyBen <boyben001@gmail.com>
2022-01-03 08:39:49 +0800

    restart

--- scan.py
+++ scan.py
@@ -1,33 +1,38 @@
 import scapy.all as scapy
-import time
+import argparse
 
-ip="192.168.62.0/24"
-clientInfo=[]
-def get_mac(ip):
-    arp_request = scapy.ARP(pdst = ip)
-    broadcast = scapy.Ether(dst ="ff:ff:ff:ff:ff:ff")
-    arp_request_broadcast = broadcast / arp_request
-    answered_list = scapy.srp(arp_request_broadcast, timeout = 5, verbose = False)[0]
-    for i,recieved in answered_list:
-        clientInfo.append({'ip':recieved.psrc,'mac':recieved.hwsrc})
+
+
+def get_argments():
+    parser = argparse.OptionParser()
+    parser.add_option("-t", "--target", dest="target",
+    help="Target IP / IP range") #當使用者在終端機執行 python3 main.py -target gateway_ip/24，期望在終端機看到輸出看到IP range。
+    options = parser.parse_args() #解析使用者輸入的值
+    return options
+
+def scan(ip):
+    #Create ARP request directed to broadcast MAC asking for IP#
+    arp_request = scapy.ARP(pdst=ip)  #only need for IP Field
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")  #利用scapy模組的Ether物件
+    arp_request_broadcast = broadcast/arp_request  #把arp_request跟broadcast混合，發送此混合封包，然後它會自動送到廣播的MAC address，去詢問我們想知道的IP位址
+    #----------------------------------------------------------#
+    
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]  #Send packet and receive response
+    
+    client_list = []
+    for element in answered_list:
+        client_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
+        client_list.append(client_dict)
+    return client_list  
+    
+    
+def print_result(list):   #Parse the response
+    print("IP\t\t\tMAC Address\n-----------------------------------------")
+    for client in list:
+        print(client["ip"] + "\t\t" + client["mac"])
+               
+      
         
-
-    return 0
-
-def spoof(target_ip, spoof_ip):
-    packet = scapy.ARP(op = 2, pdst = target_ip, 
-                     hwdst = get_mac(target_ip), 
-                               psrc = spoof_ip)
-  
-    scapy.send(packet, verbose = False)
-
-#arp_request = scapy.ARP(pdst = ip)
-#broadcast = scapy.Ether(dst ="ff:ff:ff:ff:ff:ff")
-#arp_request_broadcast = broadcast / arp_request
-#answered_list = scapy.srp(arp_request_broadcast, timeout = 5, verbose = False)[0]
-
-
-get_mac(ip)
-for iter in clientInfo:
-    print(iter['ip']+' || '+iter['mac'])
-#print(hwdst)
+gateway_ip = "192.168.0.1/24"   
+scan_result = scan(gateway_ip)    #可拿到所有子網路的封包回應
+print_result(scan_result)
